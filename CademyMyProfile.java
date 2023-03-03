f71c11ba40fea430b3e35173b324a0eaadfbf6cf
LAPTOP-OTL5I1EA\amari <amariwty@gmail.com>
2023-03-03 10:37:04 +0900

    Codecademy Practice

---
+++ CademyMyProfile.java
@@ -0,0 +1,19 @@
+public class CademyMyProfile {
+	public static void main(String[] args) {   
+  String name ="tenyee";
+  //字符串接双引号里面无限量字符
+  
+        int age = 23;
+      //int设置为一个整数
+      
+  double desiredSalary = 50000000.00;
+//double接一个大整数精确到小数点后两位
+
+  char gender = 'm';
+//char接单引号里面只能一个字符
+
+  boolean lookingForJob = true;
+//boolean判别true和false
+
+	}
+}
